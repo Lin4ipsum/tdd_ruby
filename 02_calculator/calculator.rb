6fa4d31ec3098830b442c5a8183f8115893463e4
Linda <lin.m.gonzalez@gmail.com>
2013-01-09 12:20:06 -0500

    complete 02_calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -13,3 +13,19 @@ def sum(array)
     array.inject { |sum, n| sum + n }
   end
 end
+
+def multiply(array)
+  array.reduce(:*)
+end
+
+def power(num1, num2)
+  num1**num2
+end
+
+def factorial(num)
+  if num == 0
+    0
+  else
+    (1..num).reduce(:*)
+  end
+end
