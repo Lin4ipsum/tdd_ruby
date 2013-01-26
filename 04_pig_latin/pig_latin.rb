47d2ff6e6f55505f14af278e88fb637d5f2ecb5e
Linda <lin.m.gonzalez@gmail.com>
2013-01-26 15:13:24 -0500

    1st attempt

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,14 +1,16 @@
 def translate(string)
-  if string.match(/\A[aeiou]/)
-    begin_w_vowel(string)
-  elsif string[1].match(/\A[aeiou]/)
+  if string.match(/\A[aeio]/)
+    vowel(string)
+  elsif string[1].match(/\A[aeio]/)
     one_consonant(string)
-  else
+  elsif string.match(/^[qu]/) or string[2].match(/\A[aeio]/)
     two_consonant(string)
+  else string[2].match(/\A[aeio]/)
+    three_consonant(string)
   end
 end
 
-def begin_w_vowel(string)
+def vowel(string)
   string + "ay"
 end
 
@@ -20,10 +22,52 @@ def two_consonant(string)
   string[2..-1] + string[0..1] + "ay"
 end
 
+def three_consonant(string)
+  string[3..-1] + string[0..2] + "ay"
+end
 
-  it "translates two words" do
-    s = translate("eat pie")
-    s.should == "eatay iepay"
+def translate_words(string)
+  words = string.split #array
+  puts words
+  # x = 0
+  # p = ""
+  # while x <= words.count
+  #   p << translate(words[0]) + " "
+  #   x+=1
   end
 
+
+# def translate_words(string)
+#   words = string.split
+#   for i in 0..words.count do
+#     puts translate(words[i]) + " "
+#     i+=1
+#   end
+# end
+
+# end
+
+# split the string up on the spaces
+# turn each word into a string
+
+# split string into 2 strings on the space
+# "eat pie" => "eat" "pie"
+# put strings through translate
+# then join the stringds
+
+# def two_words(string)
+#   if string.match(/\s+/)
+#     s = string.split
+#     s.fetch
+
+
+
+#   it "translates two words" do
+#     s = translate("eat pie")
+#     s.should == "eatay iepay"
+#   end
+
+# it "counts 'qu' as a single phoneme" do
+#     s = translate("quiet")
+#     s.should == "ietquay"
  
